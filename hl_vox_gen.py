cefc9a1ec7fc5b6346f761a5f28ea633be1bbc4f
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-01 20:43:36 +0000

    add -p option

--- hl_vox_gen.py
+++ hl_vox_gen.py
@@ -1,18 +1,16 @@
 import sys
-import wave
 import os
-def out_gen(infiles, outfile, cwd):
-    data = []
-    for infile in infiles:
-        w = wave.open(infile, 'rb')
-        data.append( [w.getparams(), w.readframes(w.getnframes())] )
-        w.close()
+from pydub import AudioSegment
+from pydub.playback import play
+def out_gen(infiles, outfile, cwd, pl):
+    sound = AudioSegment.from_wav(infiles[0])
+    for filenum in range(1, len(infiles)):
+        sound += AudioSegment.from_wav(infiles[filenum])
     os.chdir(cwd)
-    output = wave.open(outfile, 'wb')
-    output.setparams(data[0][0])
-    for i in range(len(data)):
-        output.writeframes(data[i][1])
-    output.close()
+    if pl != 2:
+        sound.export(outfile, format="wav")
+    if pl != 1:
+        play(sound)
 
 def main():
     cwd = os.getcwd()
@@ -22,12 +20,15 @@ def main():
     outfile = "out.wav"
     error_flag = True
     options = []
+    pl = 0
     if arg[-1] == "--help" or arg[-1] == "-h":
         print("Half life vox generator\n")
         print("usage: python3 hl_vox_gen [option(s)] [file(s)]")
         print("supported optins [default in brackets]:")
         print("-v use different directory for .wav files [vox]")
         print("-o specify the name of the output file [out.wav]")
+        print("-p play file after generating if 0, only generate if one")
+        print("only play if 2 [0]")
         print("-h or --help print this message and exit")
     else:
         new_arg = []
@@ -50,6 +51,11 @@ def main():
                         new_arg.pop(argnum - offset)
                         new_arg.pop(argnum - offset)
                         offset += 2
+                elif arg[argnum] == "-p":
+                    pl = int(arg[argnum + 1])
+                    new_arg.pop(argnum - offset)
+                    new_arg.pop(argnum - offset)
+                    offset += 2
         if error_flag:
             os.chdir(vox_dir)
             vox_words = os.listdir()
@@ -64,7 +70,8 @@ def main():
                     break
             print("arguments: " + "".join(word + " " for word in arg_new) + f"\noutput file: {outfile}\nvoxdir: {vox_dir[2:]}")
             if error_flag:
-                out_gen(arg_new, outfile, cwd)
+                out_gen(arg_new, outfile, cwd, pl)
+                print("Success")
 
 if __name__ == "__main__":
     main()
