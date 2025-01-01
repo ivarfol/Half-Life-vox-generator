c56e1a127a082d2fd6694dc6ff10922d710dc657
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-01 17:56:47 +0000

    more command line options

--- hl_vox_gen.py
+++ hl_vox_gen.py
@@ -17,28 +17,50 @@ def out_gen(infiles, outfile, cwd):
 
 def main():
     cwd = os.getcwd()
-    os.chdir(os.path.expanduser("~/.local/share/Steam/steamapps/common/Half-Life/valve/sound/vox/"))
+    os.chdir(os.path.expanduser("~/.local/share/Steam/steamapps/common/Half-Life/valve/sound/"))
+    vox_dir = "./vox"
     arg = sys.argv[1:]
     outfile = "out.wav"
     error_flag = True
+    options = []
     if arg[-1] == "--help" or arg[-1] == "-h":
         print("help")
     else:
-        if arg[-2] == "-o":
-            outfile= arg[-1]
-            arg = arg[:-2]
-        vox_words = os.listdir()
-        for argument in arg:
-            if not argument + ".wav" in vox_words:
-                print(f"invalid argument {argument}.wav does not exist")
-                error_flag = False
-                break
-        print(f"arguments: {arg}\noutput file: {outfile}")
+        new_arg = []
+        new_arg.extend(arg)
+        offset = 0
+        for argnum in range(len(arg)):
+            if arg[argnum][0] == "-":
+                if arg[argnum] == "-o":
+                    outfile = arg[argnum + 1]
+                    new_arg.pop(argnum - offset)
+                    new_arg.pop(argnum - offset)
+                    offset += 2
+                elif arg[argnum] == "-v":
+                    if not arg[argnum + 1] in os.listdir():
+                        print(arg[argnum + 1], "is not a directory")
+                        error_flag = False
+                        break
+                    else:
+                        vox_dir = "./" + arg[argnum + 1]
+                        new_arg.pop(argnum - offset)
+                        new_arg.pop(argnum - offset)
+                        offset += 2
         if error_flag:
+            os.chdir(vox_dir)
+            vox_words = os.listdir()
             arg_new = []
-            for argument in arg:
-                arg_new += [argument + ".wav"]
-            out_gen(arg_new, outfile, cwd)
+            for argument in new_arg:
+                argument += ".wav"
+                if argument in vox_words:
+                    arg_new += [argument]
+                else:
+                    print(f"invalid argument {argument} does not exist")
+                    error_flag = False
+                    break
+            print(f"arguments: {arg_new}\noutput file: {outfile}\nvoxdir: {vox_dir[2:]}")
+            if error_flag:
+                out_gen(arg_new, outfile, cwd)
 
 if __name__ == "__main__":
     main()
