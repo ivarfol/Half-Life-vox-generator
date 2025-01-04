ae795f410b4cab622811d9cfd333248e87aa6687
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-04 22:06:37 +0000

    better values for pitch

--- hl_vox_gen.py
+++ hl_vox_gen.py
@@ -54,9 +54,9 @@ def postcontrol(infile, control_arr):
     if control_arr[0] != 100:
         sound = sound[:control_arr[0] - 100]
     if control_arr[2] != 0:
-        sound = sound[control_arr[2]:] # cut start, 100 = 0.1s
+        sound = sound[control_arr[2]*10:] # cut start, 100 = 0.1s
     if control_arr[1] != 0:
-        octaves = control_arr[1] / 255 # change pitch, 255 = 1 octave
+        octaves = (control_arr[1] - 100) / 50 # change pitch, 255 = 1 octave
         new_sample_rate = int(sound.frame_rate * (2 ** octaves))
         hipitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
         hipitch_sound = hipitch_sound.set_frame_rate(11025)
