6c6030ccf40724d2b08b80c69ab9f79636dd94c8
iaroslav <varfolomeeviaroslav@gmail.com>
2025-01-10 09:30:03 +0000

    add a script for copying files from the steam version, so that program will work when half life is uninstalled

---
+++ copy_files.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+mkdir sound
+cd sound
+mkdir valve
+mkdir gearbox
+mkdir bshift
+cp -r ~/.local/share/Steam/steamapps/common/Half-Life/valve/sound ./valve
+cp -r ~/.local/share/Steam/steamapps/common/Half-Life/gearbox/sound ./gearbox
+cp -r ~/.local/share/Steam/steamapps/common/Half-Life/bshift/sound ./bshift
